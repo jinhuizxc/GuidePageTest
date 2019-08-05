d86f4973dd745fafb0474d75b3f6195e9fafe414
j_h <13113017500>
2019-08-05 11:45:00 +0800

    Initial commit

---
+++ app/src/main/java/com/zxxxjs/android/guidepagetest/welcome_videopager/WelcomeVideoPage.java
@@ -0,0 +1,15 @@
+package com.zxxxjs.android.guidepagetest.welcome_videopager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zxxxjs.android.guidepagetest.R;
+
+public class WelcomeVideoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_videopager);
+    }
+}
